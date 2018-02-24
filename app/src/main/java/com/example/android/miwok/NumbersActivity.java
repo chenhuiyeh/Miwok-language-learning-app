c187863b7671e1632c14a8b8ad6d22e642949c29
Chen-Hui Yeh <chenhuiyeh@gmail.com>
2018-02-24 15:39:27 -0500

    switch to ListView

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,7 +18,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -42,15 +44,11 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.root_view);
-//        TextView wordView = new TextView (this);
-//        wordView.setText(words.get(0));
-//        rootView.addView(wordView);
+        ArrayAdapter<String> wordViewsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,words);
 
-        for (int index = 0; index < words.size(); index++) {
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(index));
-            rootView.addView(wordView);
-        }
+        ListView list = (ListView) findViewById(R.id.list);
+        list.setAdapter(wordViewsAdapter);
+        
     }
 }
